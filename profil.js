3ce19a6c027a007ba6adc37e9e0528b35ca1ab16
Legaseh <itvalentinohose@gmail.com>
2025-05-08 17:07:05 +0700

    Add files via upload
    Di Rumah

--- profil.js
+++ profil.js
@@ -1,6 +1,16 @@
 function update(){
-
     let nama = document.getElementById("nama").value;
     let gantiNama = document.getElementById("gantiNama");
-    gantiNama.innerHTML = nama;nn
+    gantiNama.innerHTML = nama;
+    let bio = document.getElementById("bio").value;
+    let gantiBio = document.getElementById("gantiBio");
+    gantiBio.innerHTML = bio;
+    let gambar = document.getElementById("gambar").value;
+    let gantiGambar = document.getElementById("gantiGambar");
+    gantiGambar.src = gambar;
+}
+function reset(){
+    document.getElementById("gantiNama").innerHTML = "John Doe";
+    document.getElementById("gantiBio").innerHTML = "Web Developer";
+    document.getElementById("gantiGambar").src = "Gambar/pict1.jpg";
 }
