aca6f127956a8ebe0c8783fcffe33ce299b694eb
Legaseh <itvalentinohose@gmail.com>
2025-05-08 11:49:30 +0700

    Update profil.js
    di kelas

--- profil.js
+++ profil.js
@@ -2,5 +2,5 @@ function update(){
 
     let nama = document.getElementById("nama").value;
     let gantiNama = document.getElementById("gantiNama");
-    gantiNama.innerHTML = nama;
+    gantiNama.innerHTML = nama;nn
 }
